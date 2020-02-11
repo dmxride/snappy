f1a619517d78c3b715f184612d8546f2754754b8
Carlos Silva <csilva@ubiwhere.pt>
2020-02-11 12:53:07 +0000

    feat: isNumber validation

--- SnappyForm/validations.js
+++ SnappyForm/validations.js
@@ -9,3 +9,8 @@ export const isEmail = (value) => {
 	const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 	return regex.test(value);
 }
+
+export const isNumber = (value) => {
+	if (!value) return false
+	return !isNaN(parseFloat(value))
+}
