568073b6bf8f96916dfcdd34ee95a59f56c1510c
Nuno Gago <ngago@ubiwhere.pt>
2020-03-09 10:34:46 +0000

    feat: get stored data

---
+++ utils.js
@@ -0,0 +1,25 @@
+function deepParseJson(jsonString) {
+  if (typeof jsonString === 'string') {
+    if (!isNaN(Number(jsonString))) {
+      return jsonString;
+    }
+    try {
+      return deepParseJson(JSON.parse(jsonString));
+    } catch (err) {
+      return jsonString;
+    }
+  } else if (Array.isArray(jsonString)) {
+    return jsonString.map(val => deepParseJson(val));
+  } else if (typeof jsonString === 'object' && jsonString !== null) {
+    return Object.keys(jsonString).reduce((obj, key) => {
+      obj[key] = deepParseJson(jsonString[key]);
+      return obj;
+    }, {});
+  } else {
+    return jsonString;
+  }
+}
+
+export default {
+  deepParseJson
+}
